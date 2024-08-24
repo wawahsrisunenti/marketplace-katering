aec1f71ada8c8d99814a107baca39d2f1af17681
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-08-25 06:56:24 +0700

    feat(frontend): create Home and Order components with API integration
    Developed Home component to display a list of merchants fetched from the API. Created Order component to allow users to place an order by submitting merchant ID, delivery date, and total price via API.

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h2>List of Merchants</h2>
+      <ul>
+        <li v-for="merchant in merchants" :key="merchant.id">
+          {{ merchant.company_name }} - {{ merchant.address }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        merchants: []
+      };
+    },
+    mounted() {
+      axios.get('/api/merchants')
+        .then(response => {
+          this.merchants = response.data;
+        });
+    }
+  };
+  </script>
+  
---
+++ resources/js/components/Order.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <h2>Order Food</h2>
+      <form @submit.prevent="placeOrder">
+        <input v-model="merchant_id" type="number" placeholder="Merchant ID" />
+        <input v-model="delivery_date" type="date" placeholder="Delivery Date" />
+        <input v-model="total_price" type="number" placeholder="Total Price" />
+        <button type="submit">Place Order</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        merchant_id: '',
+        delivery_date: '',
+        total_price: ''
+      };
+    },
+    methods: {
+      placeOrder() {
+        axios.post('/api/order', {
+          merchant_id: this.merchant_id,
+          delivery_date: this.delivery_date,
+          total_price: this.total_price
+         }).then(response => {
+           alert('Order placed successfully!');
+         }).catch(error => {
+           console.error('Error placing order:', error);
+         });
+       }
+     }
+   };
+   </script>
+  
