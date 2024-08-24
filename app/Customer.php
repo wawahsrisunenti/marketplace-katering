622128675f2d8a159e03963499e950ec82c0e419
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-08-24 22:18:37 +0700

    feat(database): add Order, Merchant,customer model and migration

--- app/Customer.php
+++ app/Customer.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Merchant extends Model
+class Customer extends Model
 {
     // Indicate the table associated with the model
-    protected $table = 'merchants';
+    protected $table = 'customers';
 
     // The attributes that are mass assignable
-    protected $fillable = ['company_name', 'address', 'contact', 'description'];
+    protected $fillable = ['name', 'email'];
 
     // Define the relationship with the Order model
     public function orders()
     {
-        return $this->hasMany(Order::class, 'merchant_id');
+        return $this->hasMany(Order::class, 'customer_id');
     }
 }
--- app/Order.php
+++ app/Order.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    // Indicate the table associated with the model
+    protected $table = 'orders';
+
+    // The attributes that are mass assignable
+    protected $fillable = ['customer_id', 'merchant_id', 'delivery_date', 'total_price'];
+
+    // Define the relationship with the Customer model
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+
+    // Define the relationship with the Merchant model
+    public function merchant()
+    {
+        return $this->belongsTo(Merchant::class, 'merchant_id');
+    }
 }
